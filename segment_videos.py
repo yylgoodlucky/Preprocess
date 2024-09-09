3f40ebeeaa117e43484b6c8226e3d5b38560c48c
yylgoodlucky <lyy2511724912@163.com>
2024-09-09 14:47:17 +0800

    segment videos

--- segment_videos.py
+++ segment_videos.py
@@ -7,7 +7,7 @@ from os.path import join, basename, dirname
 command = '/usr/bin/ffmpeg -i {} -vcodec libx264 -preset slower -profile:v high -crf 18 -pix_fmt yuv420p -loglevel quiet -segment_time 00:00:10 -f segment {}_%03d.mp4'
 
 if __name__ == '__main__':
-    dpath = '/data/test-db/home/liyongyuan/Datasets/height_crop_bilibili/videos'
+    dpath = '/data/test-db/home/liyongyuan/Datasets/height_crop_tiktok/videos'
     videos_list = glob(join(dpath, '*.mp4'))
     
     for video_path in tqdm(videos_list):
